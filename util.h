caf73fe49e61ebcb90431e3d89f4f5ac89dc7d16
RadWolfie <coolest2guy@server.fake>
2014-09-20 15:41:40 -0500

    Refixed, forgot to fix it in Add-on API's file.

--- util.h
+++ util.h
@@ -103,7 +103,7 @@
             while(scan) {
                 d_stack* newStack = (d_stack*)util::AllocMem(sizeof(d_stack));
                 newStack->d_type = (T*)util::AllocMem(sizeof(T));
-                *newStack->d_type = *clone->d_type;
+                *newStack->d_type = *scan->d_type;
                 newStack->next_d_type = NULL;
                 clone->next_d_type = newStack;
                 clone = newStack;
